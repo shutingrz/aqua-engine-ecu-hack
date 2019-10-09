9c78ad7134f64006d7e6d42c76df503bf5f6521b
shutingrz <shutingrz@gmail.com>
2019-10-08 22:06:52 -0700

    TargetCalibration総当りスクリプト

--- change_reprosession.py
+++ change_reprosession.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import time
+import time, sys
 import config as CarConfig
 import aqua_patch
 
@@ -16,13 +16,13 @@ import random, datetime
 
 #udsoncan.setup_logging()
 
-isotp_socket = isotp.socket()
-isotp_socket.set_opts(txpad=CarConfig.TXPADSIZE, rxpad=CarConfig.RXPADSIZE)
-
 aqua_patch.PatchMessageFormat()
 
-if __name__ == '__main__':
 
+
+if __name__ == '__main__':
+    isotp_socket = isotp.socket()
+    isotp_socket.set_opts(txpad=CarConfig.TXPADSIZE, rxpad=CarConfig.RXPADSIZE)
     conn = IsoTPSocketConnection('can0', rxid=CarConfig.RXID, txid=CarConfig.TXID, tpsock=isotp_socket)
 
     with Client(conn, request_timeout=2, config=CarConfig.car_client_config) as client:
@@ -30,9 +30,7 @@ if __name__ == '__main__':
             client.change_session(DiagnosticSessionControl.Session.defaultSession)
             client.unlock_security_access(CarConfig.SECURITY_LEVEL)
             client.change_session(DiagnosticSessionControl.Session.programmingSession)   
-            
-                
-            
+
         except NegativeResponseException as e:
             print('Server refused our request for service %s with code "%s" (0x%02x)' % (e.response.service.get_name(), e.response.code_name, e.response.code))
         except UnexpectedResponseException as e:
@@ -40,4 +38,37 @@ if __name__ == '__main__':
         except TimeoutException as e:
             pass
 
+    isotp_socket2 = isotp.socket()
+    isotp_socket2.set_opts(txpad=CarConfig.TXPADSIZE, rxpad=CarConfig.RXPADSIZE)
+    conn2 = IsoTPSocketConnection('can0', rxid=0x002, txid=0x001, tpsock=isotp_socket2)
+
+    with Client(conn2, request_timeout=2, config=CarConfig.car_client_config) as client:
+        try:
+            conn2.send(b'\x20\x07\x01\x00\x02\x00')
+            conn2.wait_frame(timeout=0.04)
+            conn2.send(b'\x07\x00')
+            conn2.wait_frame(timeout=0.04)
+
+            before_time = time.time()
+            for cnt in range(0, 0xffffffff):
+                conn2.send(cnt.to_bytes(4, "big"))
+                payload = conn2.wait_frame(timeout=0.004)
+                
+                if payload is not None:
+                    print("bingo! password: %s, payload: %s" % (format(cnt, '#010x'), payload))
+                    sys.exit()
+
+                if cnt % 100 == 0:
+                    remain = 0xffffffff - cnt
+                    etaTime = datetime.timedelta(seconds=int(((time.time() -before_time)/100) * remain))
+                    before_time = time.time()
+                    print("\r%s / %s (cand: %s, ETA(s): %s)" % (cnt, 0xffffffff, format(cnt, '#010x'), etaTime), end="")
+
+
+        except NegativeResponseException as e:
+            print('Server refused our request for service %s with code "%s" (0x%02x)' % (e.response.service.get_name(), e.response.code_name, e.response.code))
+        except UnexpectedResponseException as e:
+            print('Server sent an invalid payload : %s' % e.response.original_payload)
+        except TimeoutException as e:
+            pass
 
